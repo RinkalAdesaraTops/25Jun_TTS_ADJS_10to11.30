bcc4fc63228654588b02cf39c625bea6c4514e81
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-07-18 12:54:29 +0530

    dom

---
+++ Lecture3/task.js
@@ -0,0 +1,33 @@
+Task
+// 1)const arr = [1,2,3,4,5]
+//Add 10 value in every elements
+//Find only even no
+
+let user_arr = [
+    {
+        id:"101",
+        name:"jhdfs",
+        age:25,
+        salary:22000
+    },
+    {
+        id:"102",
+        name:"test",
+        age:22,
+        salary:13500
+    },
+    {
+        id:"103",
+        name:"abc",
+        age:18,
+        salary:26000
+    }
+]
+
+
+//I want to print only name & age
+//Search those users whose salary is minimum 15000
+//Search those users whose age is minimum 20
+
+let num = [5,20,100,60,1];
+//Find maximum value
